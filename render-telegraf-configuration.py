ac708f7659f5ea3f46c0102e8ea77a0546be3c12
ksator <khelil.sator.git@gmail.com>
2019-01-02 09:04:09 -0800

    m

--- render-telegraf-configuration.py
+++ render-telegraf-configuration.py
@@ -1,17 +1,29 @@
-from pprint import pprint
+# usage:
+# python ./render-telegraf-configuration.py -h
+# python ./render-telegraf-configuration.py --help
+# python ./render-telegraf-configuration.py --output 'configs/telegraf-openconfig.conf' --template 'templates/telegraf-openconfig.j2' --yaml 'data.yml'
+# python ./render-telegraf-configuration.py -o 'configs/telegraf-openconfig.conf' -t 'templates/telegraf-openconfig.j2' -y 'data.yml'
+
 import yaml
 from jinja2 import Template
+import argparse
 
-# print 'Start building telegraf configuration'
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--output', help='rendered file')
+parser.add_argument('-t', '--template', help='jinja template file')
+parser.add_argument('-y', '--yaml', help='YAML variables file')
+args = parser.parse_args()
 
-with open('data.yml') as f:
+# Start building telegraf configuration'
+
+with open(args.yaml) as f:
   data=yaml.load(f.read())
 
-with open('templates/telegraf-openconfig.j2') as f:
+with open(args.template) as f:
   template = Template(f.read())
 
-conf=open('configs/telegraf-openconfig.conf','w')
+conf=open(args.output,'w')
 conf.write(template.render(data))
 conf.close()
 
-# print 'done'
+
