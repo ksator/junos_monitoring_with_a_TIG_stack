6f347ae0495aa018cc592fa0bc2cf0348e9c6500
ksator <khelil.sator.git@gmail.com>
2019-01-02 15:19:29 -0800

    m

--- render-telegraf-configuration.py
+++ render-telegraf-configuration.py
@@ -1,9 +1,21 @@
 # usage:
+#
+# Update devices details:
 # vi data.yml
+#
+# To get help:
 # python ./render-telegraf-configuration.py -h
 # python ./render-telegraf-configuration.py --help
+#
+# Generate Telegraf configuration file for Openconfig:
 # python ./render-telegraf-configuration.py --output 'configs/telegraf-openconfig.conf' --template 'templates/telegraf-openconfig.j2' --yaml 'data.yml'
 # python ./render-telegraf-configuration.py -o 'configs/telegraf-openconfig.conf' -t 'templates/telegraf-openconfig.j2' -y 'data.yml'
+#
+# Generate Telegraf configuration file for SNMP:
+# python ./render-telegraf-configuration.py --output 'configs/telegraf-snmp.conf' --template 'templates/telegraf-snmp.j2' --yaml 'data.yml'
+# python ./render-telegraf-configuration.py -o 'configs/telegraf-snmp.conf' -t 'templates/telegraf-snmp.j2' -y 'data.yml'
+
+
 
 import yaml
 from jinja2 import Template
